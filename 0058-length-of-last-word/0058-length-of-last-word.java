43d7a2e4d3d311cd4b60aae76860ec11f3d68570
Priyal Jain <163632872+priyal1350@users.noreply.github.com>
2026-02-03 09:57:55 +0530

    Time: 0 ms (100%), Space: 43 MB (65.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,34 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         s = s.trim();   // remove extra spaces
+//         int count = 0;
+
+//         for (int i = s.length() - 1; i >= 0; i--) {
+//             if (s.charAt(i) != ' ')
+//                 count++;
+//             else
+//                 break;
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();   // remove extra spaces
+        int i = s.length() - 1;
         int count = 0;
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ')
-                count++;
-            else
-                break;
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
+
+        // Count last word characters
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+
         return count;
     }
 }
+
