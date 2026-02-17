603b8db0ec0a198bd5a24a82a28692d070908579
Priyal Jain <163632872+priyal1350@users.noreply.github.com>
2026-02-17 23:37:15 +0530

    Time: 6 ms (42.59%), Space: 44.4 MB (75.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,37 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        return Arrays.equals(a, b);
+        if(s.length()!=t.length()) return false;
+        int count[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+             count[t.charAt(i)-'a']--;
+        }
+        for(int i:count){
+            if(i!=0) return false;
+        }
+        return true;
     }
 }
-// int[] freq = new int[26];
-
-// for (char c : s.toCharArray()) freq[c - 'a']++;
-// for (char c : t.toCharArray()) freq[c - 'a']--;
-
-// for (int x : freq)
-//     if (x != 0) return false;
-
-// return true;
-// public boolean isAnagram(String s, String t) {
-//     if (s.length() != t.length()) return false;
-
-//     int[] count = new int[26];
-
-//     for (int i = 0; i < s.length(); i++) {
-//         count[s.charAt(i) - 'a']++;
-//         count[t.charAt(i) - 'a']--;
-//     }
-
-//     for (int c : count) {
-//         if (c != 0) return false;
-//     }
-//     return true;
-// }
