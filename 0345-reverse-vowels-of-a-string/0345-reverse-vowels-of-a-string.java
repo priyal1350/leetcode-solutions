70b9a06ad11592b5d04d85b44168c57ed5a29d44
Priyal Jain <163632872+priyal1350@users.noreply.github.com>
2026-03-28 08:21:58 +0530

    Time: 2 ms (99.41%), Space: 46.5 MB (86.48%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -33,29 +33,49 @@ class Solution {
 
         // return new String(arr);
 
-        char []arr=s.toCharArray();
-        int left=0;
-        int right=arr.length-1;
-        while(left<right){
-            while(left<right && !isVowel(arr[left])){
-                left++;
-            }
-            while(left<right && !isVowel(arr[right])){
-                right--;
-            }
-            char temp=arr[left];
-            arr[left]=arr[right];
-            arr[right]=temp;
+    //     char []arr=s.toCharArray();
+    //     int left=0;
+    //     int right=arr.length-1;
+    //     while(left<right){
+    //         while(left<right && !isVowel(arr[left])){
+    //             left++;
+    //         }
+    //         while(left<right && !isVowel(arr[right])){
+    //             right--;
+    //         }
+    //         char temp=arr[left];
+    //         arr[left]=arr[right];
+    //         arr[right]=temp;
 
+    //         left++;
+    //         right--;
+    //     }
+    //     return new String(arr);
+    // }
+    //     public boolean isVowel(char c){
+    //          return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'
+    //         ||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    //     }
+           char arr[]=s.toCharArray();
+           int left=0;
+           int right=arr.length-1;
+           while(left<right){
+            while(left<right && !isVowel(arr[left]))
             left++;
+            while(left<right && !isVowel(arr[right]))
             right--;
-        }
-        return new String(arr);
+           
+           char temp=arr[left];
+           arr[left]=arr[right];
+           arr[right]=temp;
+           left++;
+           right--;
     }
-        public boolean isVowel(char c){
-             return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'
-            ||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
-        }
-
+           return new String(arr);
+    }
+    public boolean isVowel(char c){
+       return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'
+       ||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
     }
 
+}
